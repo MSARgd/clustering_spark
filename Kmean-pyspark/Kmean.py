d0b5430797fc9be7ad00d68e8c2a0e163d774d73
sidiAbdalla <sidabdallard@gmail.com>
2023-12-22 12:20:19 +0100

    Fri Dec 22 12:20:19 PM +01 2023

--- Kmean-pyspark/Kmean.py
+++ Kmean-pyspark/Kmean.py
@@ -1,17 +1,16 @@
 import os
 from pyspark.sql import SparkSession
-from datetime import datetime
+from pyspark.ml.feature import VectorAssembler, MinMaxScaler
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
 
-date_today = datetime.now().date()
+
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
 
-spark = SparkSession.builder.appName("PySPARK").master("local[*]") \
+spark = SparkSession.builder.appName("PySPARK KMeans Clustering").master("local[*]") \
     .getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
-
-
-
-
-
-
+dataset = spark.read.option("inferSchema", True).option("header", True).csv("Mall_Customers.csv")
+vectorAssembler = VectorAssembler(inputCols=["Age", "Annual Income (k$)", "Spending Score (1-100)"], outputCol="Features")
+assambledDataset = vectorAssembler.transform(dataset)
